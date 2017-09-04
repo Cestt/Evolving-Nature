71cf841d7adfed7762c017fbdb6bd6d0ca8abd03
Cestt <electromanuel33@gmail.com>
2017-09-04 03:15:37 +0200

    NR V0.01 Procedural Terrain
    Added Peril noise map generation with mesh and heigth

---
+++ Evolving Nature/Assets/Scripts/Map/Noise.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Noise {
+
+	public static float[,] GenerateNoiseMap(int mapWidth, int mapHeigth, int seed, float scale, int octaves, float persistance, float lacunarity, Vector2 offset){
+
+		float[,] noisemap = new float[mapWidth, mapHeigth];
+
+		System.Random prng = new System.Random (seed);
+		Vector2[] octaveOffSets = new Vector2[octaves];
+
+		for (int i = 0; i < octaves; i++) {
+			float offsetX = prng.Next (-100000, 100000) + offset.x;
+			float offsetY = prng.Next (-100000, 100000) + offset.y;
+			octaveOffSets [i] = new Vector2 (offsetX, offsetY);
+		}
+
+		if (scale <= 0)
+			scale = 0.0001f;
+
+		float maxNoiseHeigth = float.MinValue;
+		float minNoiseHeigth = float.MaxValue;
+
+		float halfWidth = mapWidth / 2f;
+		float halfHeigth = mapHeigth / 2f;
+		
+		for (int y = 0; y < mapHeigth; y++) {
+			for (int x = 0; x < mapWidth; x++) {
+
+				float amplitude = 1;
+				float frequency = 1;// More frequency more changes in the values(terrain)
+				float noiseHeigth = 0;
+
+				for (int i = 0; i < octaves; i++) {
+					float sampleX = (x - halfWidth) / scale * frequency + octaveOffSets[i].x;
+					float sampleY = (y - halfHeigth) / scale * frequency + octaveOffSets[i].y;
+
+					float perlinValue = Mathf.PerlinNoise (sampleX, sampleY) * 2 - 1;// Values between 0 and 1 we multiply by 2 and sustract 1 to make values between -1  and 1;
+					noiseHeigth += perlinValue * amplitude;
+
+					amplitude *= persistance;
+					frequency *= lacunarity;
+				}
+				if (noiseHeigth > maxNoiseHeigth) {
+					maxNoiseHeigth = noiseHeigth;
+				} else if (noiseHeigth < minNoiseHeigth) {
+					minNoiseHeigth = noiseHeigth;
+				}
+
+				noisemap [x, y] = noiseHeigth;
+
+			}
+		}
+
+		for (int y = 0; y < mapHeigth; y++) {
+			for (int x = 0; x < mapWidth; x++) {
+				noisemap [x, y] = Mathf.InverseLerp (minNoiseHeigth, maxNoiseHeigth, noisemap [x, y]);
+			}
+		}
+
+		return noisemap;
+	}
+}
---
+++ Evolving Nature/Assets/Scripts/Map/TextureGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class TextureGenerator {
+
+	public static Texture2D TextureFromColorMap(Color[] colormap, int width, int heigth){
+		Texture2D texture = new Texture2D (width, heigth);
+
+		texture.filterMode = FilterMode.Point;
+		texture.wrapMode = TextureWrapMode.Clamp;
+		texture.SetPixels (colormap);
+		texture.Apply ();
+		return texture;
+	}
+
+	public static Texture2D TextureFromHeigthMap(float[,] heigthmap){
+		int width = heigthmap.GetLength (0);
+		int heigth = heigthmap.GetLength (1);
+
+		Color[] colorMap = new Color[width * heigth];
+		for (int i = 0; i < heigth; i++) {
+			for (int j = 0; j < width; j++) {
+				colorMap [i * width + j] = Color.Lerp (Color.black, Color.white, heigthmap [j, i]);
+			}
+		}
+
+		return TextureFromColorMap (colorMap, width, heigth);
+	}
+}
